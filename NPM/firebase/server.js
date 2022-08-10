b527b5ae9cbcdcf35d9cf40f02fdd2003678a8d6
tjrehdrms123 <wwe5455@naver.com>
2022-08-11 01:46:38 +0900

    [ Debug ] ing.. SenderId mismatch

--- NPM/firebase/server.js
+++ NPM/firebase/server.js
@@ -1,40 +1,29 @@
-const express = require("express");
-const app = express();
-const router = express.Router();
-const { deviceToken } = "../config";
-
-app.listen(3000, function () {
-  console.log("start! express server on port 3000");
-});
-
 const admin = require("firebase-admin");
-
-let serAccount = require("../fir-ec6c4-firebase-adminsdk-re47v-096534992d.json");
+let serAccount = require("./mp-server-7d91f-firebase-adminsdk-bk8gl-1c61202f9d.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serAccount),
 });
 
-router.get("/push_send", function (req, res, next) {
-  let target_token = deviceToken;
-  //target_token은 푸시 메시지를 받을 디바이스의 토큰값입니다
+let target_token =
+  "c_5M06jBRIGJhAhmhIzQMy:APA91bGZcK4KLVadi2lEQK7ybgeY2XyWBaceOvxvc9248vrvgWLzrQ2Gbmr0ECnqvrjGcO2bKx9nb7JvtDcXSYhpsyvQ34sLsH_Ngw0hD-AUD6Zs1pz3miwDjkyjdby4UwRg59ADb_O9";
+//target_token은 푸시 메시지를 받을 디바이스의 토큰값입니다
 
-  let message = {
-    data: {
-      title: "테스트 데이터 발송",
-      body: "데이터가 잘 가나요?",
-      style: "굳굳",
-    },
-    token: target_token,
-  };
+let message = {
+  data: {
+    title: "테스트 데이터 발송",
+    body: "데이터가 잘 가나요?",
+    style: "굳굳",
+  },
+  token: target_token,
+};
 
-  admin
-    .messaging()
-    .send(message)
-    .then(function (response) {
-      console.log("Successfully sent message: : ", response);
-    })
-    .catch(function (err) {
-      console.log("Error Sending message!!! : ", err);
-    });
-});
+admin
+  .messaging()
+  .send(message)
+  .then(function (response) {
+    console.log("Successfully sent message: : ", response);
+  })
+  .catch(function (err) {
+    console.log("Error Sending message!!! : ", err);
+  });
